fdc179bd6063cd5a4a8b4e45df74c9087906dbf1
chuninh <chuninh2006@gmail.com>
2026-04-08 21:28:38 +0700

    Add files via upload

---
+++ ss4-bai1/ss4_bai1/src/main/java/com/example/ss4_bai1/service/OrderRepository.java
@@ -0,0 +1,16 @@
+package com.example.ss4_bai1.service;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderRepository {
+
+    public String getAllOrders() {
+        return "Danh sach toan bo don hang";
+    }
+
+    public String getOrderById(Long id) {
+        return "Thong tin don hang voi ID: " + id;
+    }
+}
---
+++ ss4-bai1/ss4_bai1/src/main/java/com/example/ss4_bai1/service/OrderService.java
@@ -0,0 +1,23 @@
+package com.example.ss4_bai1.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public String getAllOrders() {
+        return orderRepository.getAllOrders();
+    }
+
+    public String getOrderById(Long id) {
+        return orderRepository.getOrderById(id);
+    }
+}
